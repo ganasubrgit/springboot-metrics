c80559e082d7e7bf5566b82d0fbe2981f05f4745
Ganapathy S <gsanthanam@aus3006lt25933.local>
2023-11-28 16:36:44 -0500

    updated

---
+++ src/main/java/com/altimetrik/sre/sredemo/SreDemoApplication.java
@@ -0,0 +1,26 @@
+package com.altimetrik.sre.sredemo;
+
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SreDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SreDemoApplication.class, args);
+	}
+	
+	/**
+	 * This is required so that we can use the @Timed annotation
+	 * on methods that we want to time.
+	 * See: https://micrometer.io/docs/concepts#_the_timed_annotation
+	 */
+	@Bean
+	public TimedAspect timedAspect(MeterRegistry registry) {
+		return new TimedAspect(registry);
+	}
+
+}
---
+++ src/test/java/com/altimetrik/sre/sredemo/SreDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altimetrik.sre.sredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SreDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
